983874b9de6f72fe7d3d4c90e207a662b9761a8e
JalerPandyaMahesa <pandyamahesa@gmail.com>
2024-08-29 13:58:52 +0700

    3

--- resources/views/admin/viewPaud.blade.php
+++ resources/views/admin/viewPaud.blade.php
@@ -226,9 +226,9 @@
                                 <h4 class="header-title"></h4>
                                 <div class="mb-2">
                                     <div class="row">
-                                        <div class="col-12 text-sm-center form-inline">
-                                            <div class="form-group mr-2" style="display:none">
-                                            </div>
+                                        <div class="col-6 text-sm-center form-inline">
+                                            <!-- <div class="form-group mr-2" style="display:none">
+                                            </div> -->
                                             <div class="form-group">
                                                 <select id="filter-kecamatan" class="custom-select custom-select-sm">
                                                     <option value="">Pilih Kecamatan</option>
@@ -263,6 +263,10 @@
                                                 </select>
                                             </div>
                                         </div>
+                                        <div class="col-6 text-right">
+                                        <a href="{{ route('admin.addSekolah') }}"
+                                        class="badge badge-info pt-1 pb-1">Tambah Sekolah</a>
+                                        </div>
                                     </div>
                                 </div>
 
@@ -291,14 +295,14 @@
                                                     <td>{{ $s->bentuk_pendidikan }}</td>
                                                     <td>{{ $s->status_sekolah }}</td>
                                                     <td>#</td>
-                                                    <td><a href="{{ route('admin.showSekolah', ['sekolah_id' => $s->sekolah_id]) }}"
+                                                    <td><a href="{{ route('admin.showSekolah', $s->sekolah_id) }}"
                                                             class="badge badge-success"><i class="mdi mdi-eye"></i> View</a>
-                                                        <a href="{{ route('admin.editSekolah', ['sekolah_id' => $sekolah->sekolah_id]) }}"
+                                                        <a href="{{ route('admin.editSekolah', $s->sekolah_id) }}"
                                                             class="badge badge-warning"><i
                                                                 class="fas fa-clipboard-check "></i> Update</a>
                                                         <button class="badge badge-danger" style="border:none">
                                                             <form
-                                                                action="{{ route('admin.destroySekolah', ['sekolah_id' => $sekolah->sekolah_id]) }}"
+                                                                action="{{ route('admin.destroySekolah', $s->sekolah_id) }}"
                                                                 method="POST" style="display:inline;"><i
                                                                     class="fas fa-trash-alt "></i> Delete
                                                                 @csrf
