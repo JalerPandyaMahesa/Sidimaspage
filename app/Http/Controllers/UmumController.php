caea195b17cf0f57d7f29fd1caca05b7a87e9bb6
GalihPutraAdiansyah <galih.putra0702@gmail.com>
2024-09-03 11:42:34 +0700

    modified:   app/Http/Controllers/UmumController.php 	modified:   routes/web.php

--- app/Http/Controllers/UmumController.php
+++ app/Http/Controllers/UmumController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Sekolah;
-use App\Models\ptk;
+use App\Models\Ptk;
 use App\Models\Pesertadidik;
 use Illuminate\Http\Request;
 
@@ -12,21 +12,18 @@ class UmumController extends Controller
     public function viewPaud()
     {
         $sekolah = Sekolah::whereIn('bentuk_pendidikan', ['TK', 'SPS', 'TPA', 'RA', 'KB'])->get();
-
         return view('Home.paud', compact('sekolah'));
     }
 
     public function viewSD()
     {
-        $sekolah = Sekolah::whereIn('bentuk_pendidikan', ['TK', 'SPS', 'TPA', 'RA', 'KB'])->get();
-
+        $sekolah = Sekolah::whereIn('bentuk_pendidikan', ['SD', 'MI', 'SPK SD'])->get();
         return view('Home.sd', compact('sekolah'));
     }
 
     public function viewSMP()
     {
-        $sekolah = Sekolah::whereIn('bentuk_pendidikan', ['TK', 'SPS', 'TPA', 'RA', 'KB'])->get();
-
+        $sekolah = Sekolah::whereIn('bentuk_pendidikan', ['SMP', 'MTS', 'SPK SMP'])->get();
         return view('Home.smp', compact('sekolah'));
     }
 
@@ -47,7 +44,6 @@ class UmumController extends Controller
         $tsSmp = PesertaDidik::whereHas('sekolah', function ($query) {
             $query->whereIn('bentuk_pendidikan', ['SMP', 'MTS', 'SPK SMP']);
         })->count();
-
 
         return view('Home.index', compact('totalTk', 'totalSd', 'totalSmp', 'totalPgtk', 'tsTk', 'tsSd', 'tsSmp'));
     }
