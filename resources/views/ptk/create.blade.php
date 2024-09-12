8bfc5b36cde59f61f33ae75127bc40fb4e52a530
GalihPutraAdiansyah <galih.putra0702@gmail.com>
2024-09-12 14:14:42 +0700

    modified:   resources/views/pesertadidik/create.blade.php 	modified:   resources/views/ptk/create.blade.php

--- resources/views/ptk/create.blade.php
+++ resources/views/ptk/create.blade.php
@@ -6,43 +6,307 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Create Peserta Didik</title>
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+    <meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description" />
+    <meta content="MartDevelopers" name="author" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+
+    <!-- App favicon -->
+    <link rel="shortcut icon" href=" {{ asset('img/Asset/favicon.ico') }} ">
+
+    <!-- Plugins css -->
+    <link href=" {{ asset('assets/libs/flatpickr/flatpickr.min.css') }} " rel="stylesheet" type="text/css" />
+
+    <!-- App css -->
+    <link href=" {{ asset('assets/css/bootstrap.min.css') }} " rel="stylesheet" type="text/css" />
+    <link href=" {{ asset('assets/css/icons.min.css') }} " rel="stylesheet" type="text/css" />
+    <link href=" {{ asset('assets/css/app.min.css') }} " rel="stylesheet" type="text/css" />
+    <!-- Loading button css -->
+    <link href=" {{ asset('assets/libs/ladda/ladda-themeless.min.css') }} " rel="stylesheet" type="text/css" />
+
+    <!-- Footable css -->
+    <link href=" {{ asset('assets/libs/footable/footable.core.min.css') }} " rel="stylesheet" type="text/css" />
+
+    <!--Load Sweet Alert Javascript-->
+    <script src=" {{ asset('assets/js/swal.js') }} "></script>
 </head>
 
 <body>
-    <h1>Create Peserta Didik</h1>
-    <form action="{{ route('ptk.import', $sekolah_id) }}" method="POST" enctype="multipart/form-data">
-        @csrf
-        <div>
-            <label for="file">Import PTK:</label>
-            <input type="file" name="file" required>
+    <!-- Begin page -->
+    <div id="wrapper">
+
+        <!-- Topbar Start -->
+        <div class="navbar-custom">
+            <ul class="list-unstyled topnav-menu float-right mb-0">
+                <li class="dropdown notification-list">
+                    <a class="nav-link dropdown-toggle nav-user mr-0 waves-effect waves-light" data-toggle="dropdown"
+                        href="#" role="button" aria-haspopup="false" aria-expanded="false">
+                        <!-- <img src=" {{ asset('assets/images/users/doc-icon.png') }} " alt="pic" class="rounded-circle"> -->
+                        <span class="pro-user-name ml-1">
+                            System Administrator <i class="mdi mdi-chevron-down"></i>
+                        </span>
+                    </a>
+                    <div class="dropdown-menu dropdown-menu-right profile-dropdown ">
+                        <!-- item-->
+                        <!-- <div class="dropdown-header noti-title">
+                        <h6 class="text-overflow m-0">Welcome !</h6>
+                    </div> -->
+
+                        <!-- item-->
+                        <!-- <a href="#" class="dropdown-item notify-item">
+                        <i class="fe-user"></i>
+                        <span>My Account</span>
+                    </a> -->
+
+
+                        <!-- <div class="dropdown-divider"></div> -->
+
+                        <!-- item-->
+                        <a href="#" class="dropdown-item notify-item">
+                            <i class="fe-log-out"></i>
+                            <span>Logout</span>
+                        </a>
+                    </div>
+                </li>
+            </ul>
+
+            <!-- LOGO -->
+            <div class="logo-box">
+                <a href="dashboard" class="logo text-center">
+                    <span class="logo-lg">
+                        <img src="{{ asset('img/Asset/Logo_Sidimas2.png') }}" alt="" height="24">
+                    </span>
+                    <span class="logo-sm">
+                        <img src="{{ asset('img/Asset/Logo_Sidimas2.png') }}" alt="" height="13">
+                    </span>
+                </a>
+            </div>
+
+            <ul class="list-unstyled topnav-menu topnav-menu-left m-0">
+                <li>
+                    <button class="button-menu-mobile waves-effect waves-light">
+                        <i class="fe-menu"></i>
+                    </button>
+                </li>
+
+            </ul>
         </div>
-        <button type="submit">Upload</button>
-    </form>
+        <!-- end Topbar -->
 
-    <form action="{{ isset($ptk) ? route('ptk.update', $ptk->ptk_id) : route('ptk.store') }}" method="POST">
-        @csrf
-        @if(isset($ptk))
-            @method('PUT')
-        @endif
+        <!-- ========== Left Sidebar Start ========== -->
+        <div class="left-side-menu">
 
-        <label for="nama">Nama:</label>
-        <input type="text" id="nama" name="nama" value="{{ isset($ptk) ? $ptk->nama : '' }}" required><br>
+            <div class="slimscroll-menu">
 
-        <input type="hidden" name="sekolah_id" value="{{ $sekolah_id }}">
+                <!--- Sidemenu -->
+                <div id="sidebar-menu">
 
-        <button type="submit">{{ isset($ptk) ? 'Update' : 'Create' }}</button>
-    </form>
+                    <ul class="metismenu" id="side-menu">
+
+                        <li class="menu-title">Navigation</li>
+
+                        <li>
+                            <a href="{{ route('admin.dashboard') }}">
+                                <i class="fe-airplay"></i>
+                                <span> Dashboard </span>
+                            </a>
+
+                        </li>
+
+                        <li>
+                            <a href="javascript: void(0);">
+                                <img src="{{ asset('img/Asset/p&d.png') }}" alt="" height="14">
+                                <span style="padding: 3px;"> Paud & Dikmas </span>
+                                <span class="menu-arrow"></span>
+                            </a>
+                            <ul class="nav-second-level" aria-expanded="false">
+                                <li>
+                                    <a href="{{ route('admin.addSekolah') }}">Tambah Sekolah</a>
+                                </li>
+                                <li>
+                                    <a href="{{ route('admin.viewPaud') }}">Lihat</a>
+                                </li>
+                            </ul>
+                        </li>
+
+                        <li>
+                            <a href="javascript: void(0);">
+                                <img src="{{ asset('img/Asset/sd.png') }}" alt="" height="14">
+                                <span style="padding: 3px;"> SD </span>
+                                <span class="menu-arrow"></span>
+                            </a>
+                            <ul class="nav-second-level" aria-expanded="false">
+                                <li>
+                                    <a href="{{ route('admin.addSekolah') }}">Tambah Sekolah</a>
+                                </li>
+                                <li>
+                                    <a href="{{ route('admin.viewSd') }}">Lihat</a>
+                                </li>
+                            </ul>
+                        </li>
+
+                        <li>
+                            <a href="javascript: void(0);">
+                                <img src="{{ asset('img/Asset/smp.png') }}" alt="" height="12">
+                                <span style="padding: 3px;"> SMP </span>
+                                <span class="menu-arrow"></span>
+                            </a>
+                            <ul class="nav-second-level" aria-expanded="false">
+                                <li>
+                                    <a href="{{ route('admin.addSekolah') }}">Tambah Sekolah</a>
+                                </li>
+                                <li>
+                                    <a href="{{ route('admin.viewSmp') }}">Lihat</a>
+                                </li>
+                            </ul>
+                        </li>
+
+                        <li>
+                            <a href="javascript: void(0);">
+                                <img src="{{ asset('img/Asset/pgtk.png') }}" alt="" height="22">
+                                <span style="padding: 3px;"> PGTK </span>
+                                <span class="menu-arrow"></span>
+                            </a>
+                            <ul class="nav-second-level" aria-expanded="false">
+                                <li>
+                                    <a href="{{ route('admin.viewPgtk') }}">Lihat</a>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+
+                </div>
+                <!-- End Sidebar -->
+
+                <div class="clearfix"></div>
+
+            </div>
+            <!-- Sidebar -left -->
+
+        </div> <!-- Left Sidebar End -->
+
+        <!-- ============================================================== -->
+        <!-- Start Page Content here -->
+        <!-- ============================================================== -->
+
+        <div class="content-page">
+            <div class="content">
+
+                <!-- Start Content-->
+                <div class="container-fluid">
+
+                    <!-- start page title -->
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="page-title-box">
+                                <div class="page-title-right">
+                                    <ol class="breadcrumb m-0">
+                                        <li class="breadcrumb-item"><a
+                                                href="{{ route('admin.dashboard') }}">Dashboard</a>
+                                        </li>
+                                        <li class="breadcrumb-item"><a href="javascript: void(0);">Sekolah</a></li>
+                                        <li class="breadcrumb-item active">Add Sekolah</li>
+                                    </ol>
+                                </div>
+                                <h4 class="page-title">Add Sekolah Details</h4>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- end page title -->
+                    <!-- Form row -->
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="card">
+                                <div class="card-body">
+                                    <label class="col-form-label">Create PTK (xls, xlsx)</label>
+                                    <form action="{{ route('ptk.import', $sekolah_id) }}" method="POST" enctype="multipart/form-data">
+                                        @csrf
+                                        <div>
+                                            <label for="file">Import PTK:</label>
+                                            <input type="file" name="file" required>
+                                        </div>
+                                        <button type="submit">Upload</button>
+                                    </form>
+
+                                    <h2></h2>
+                                    <!-- <h4 class="header-title">Fill all fields</h4> -->
+                                    <!--Create Ptk Form-->
+                                    <form action="{{ isset($ptk) ? route('ptk.update', $ptk->ptk_id) : route('ptk.store') }}" method="POST">
+                                        @csrf
+                                        @if(isset($ptk))
+                                            @method('PUT')
+                                        @endif
+                                
+                                        <label for="nama">Nama:</label>
+                                        <input type="text" id="nama" name="nama" value="{{ isset($ptk) ? $ptk->nama : '' }}" required><br>
+                                
+                                        <input type="hidden" name="sekolah_id" value="{{ $sekolah_id }}">
+                                
+                                        <button type="submit">{{ isset($ptk) ? 'Update' : 'Create' }}</button>
+                                    </form>
+                                    <!--End Create Ptk Form-->
+                                </div> <!-- end card-body -->
+                            </div> <!-- end card-->
+                        </div> <!-- end col -->
+
+                    </div>
+                    <!-- end row -->
+
+                </div> <!-- container -->
+
+            </div> <!-- content -->
+
+            <!-- Footer Start -->
+            <footer class="footer">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-md-12">
+                            2024 &copy; SIDIMAS</a>
+                        </div>
+
+                    </div>
+                </div>
+            </footer>
+            <!-- end Footer -->
+
+        </div>
+
+        <!-- ============================================================== -->
+        <!-- End Page content -->
+        <!-- ============================================================== -->
+
+
+    </div>
+    <!-- END wrapper -->
 
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
     <script>
-        $(document).ready(function () {
+        $(document).ready(function() {
             $('#sekolah_id').select2({
                 placeholder: 'Select a school',
                 allowClear: true
             });
         });
     </script>
+
+    <!-- Vendor js -->
+    <script src=" {{ asset('assets/js/vendor.min.js') }} "></script>
+
+    <!-- Plugins js-->
+    <script src=" {{ asset('assets/libs/flatpickr/flatpickr.min.js') }} "></script>
+    <script src=" {{ asset('assets/libs/jquery-knob/jquery.knob.min.js') }} "></script>
+    <script src=" {{ asset('assets/libs/jquery-sparkline/jquery.sparkline.min.js') }} "></script>
+    <script src=" {{ asset('assets/libs/flot-charts/jquery.flot.js') }} "></script>
+    <script src=" {{ asset('assets/libs/flot-charts/jquery.flot.time.js') }} "></script>
+    <script src=" {{ asset('assets/libs/flot-charts/jquery.flot.tooltip.min.js') }} "></script>
+    <script src=" {{ asset('assets/libs/flot-charts/jquery.flot.selection.js') }} "></script>
+    <script src=" {{ asset('assets/libs/flot-charts/jquery.flot.crosshair.js') }} "></script>
+
+    <!-- Dashboar 1 init js-->
+    <script src=" {{ asset('assets/js/pages/dashboard-1.init.js') }} "></script>
+
+    <!-- App js-->
+    <script src=" {{ asset('assets/js/app.min.js') }} "></script>
 </body>
 
 </html>
