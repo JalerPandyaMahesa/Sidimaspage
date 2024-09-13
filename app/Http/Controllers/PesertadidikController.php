f3b0cf7c622a69f26a521834e8b178476ae8970f
Yricneilson <kapoktolol@gmail.com>
2024-09-13 07:52:41 +0700

    modified:   app/Http/Controllers/PesertadidikController.php 	modified:   app/Models/Pesertadidik.php 	modified:   composer.lock 	modified:   resources/views/pesertadidik/create.blade.php

--- app/Http/Controllers/PesertadidikController.php
+++ app/Http/Controllers/PesertadidikController.php
@@ -27,7 +27,7 @@ class PesertadidikController extends Controller
         $sekolah = Sekolah::where('bentuk_pendidikan', 'TK')
             ->orWhere('bentuk_pendidikan', 'PAUD')
             ->get();
- 
+
         return view('sekolah.filter', ['sekolah' => $sekolah, 'filter' => 'TK']);
     }
 
@@ -54,19 +54,115 @@ class PesertadidikController extends Controller
         $sekolah = Sekolah::all(); // Fetch all sekolah records
         return view('pesertadidik.create', compact('sekolah', 'sekolah_id'));
     }
-    
+
 
     public function store(Request $request)
     {
         $validatedData = $request->validate([
             'nama' => 'required|string|max:255',
+            'nisn' => 'required|string|max:10',
+            'nik' => 'required|string|max:16',
+            'no_kk' => 'required|string|max:16',
+            'tempat_lahir' => 'required|string|max:255',
+            'tanggal_lahir' => 'date',
+            'jenis_kelamin' => 'required|string|max:1',
+            'agama' => 'required|string|max:50',
+            'nama_ayah' => 'required|string|max:255',
+            'nik_ayah' => 'required|string|max:16',
+            'tahun_lahir_ayah' => '',
+            'jenjang_pendidikan_ayah_keterangan' => 'nullable|string|max:255',
+            'pekerjaan_ayah' => 'nullable|string|max:255',
+            'nama_ibu_kandung' => 'required|string|max:255',
+            'nik_ibu' => 'required|string|max:16',
+            'tahun_lahir_ibu' => '',
+            'jenjang_pendidikan_ibu_keterangan' => 'nullable|string|max:255',
+            'pekerjaan_ibu' => 'nullable|string|max:255',
+            'semester_id' => 'required|integer',
+            'rombongan_belajar_id' => 'required|integer',
+            'anggota_rombel_id' => 'required|integer',
+            'agama_id' => 'required|integer',
+            'alamat_jalan' => 'required|string|max:255',
+            'rt' => 'required|string|max:5',
+            'rw' => 'required|string|max:5',
+            'nama_dusun' => 'nullable|string|max:255',
+            'desa_kelurahan' => 'nullable|string|max:255',
+            'kode_wilayah' => 'required|string|max:10',
+            'kode_kecamatan' => 'required|string|max:10',
+            'kecamatan' => 'required|string|max:255',
+            'kode_kabupaten' => 'required|string|max:10',
+            'kabupaten' => 'required|string|max:255',
+            'kode_provinsi' => 'required|string|max:10',
+            'provinsi' => 'required|string|max:255',
+            'kode_pos' => 'nullable|string|max:5',
+            'lintang' => '',
+            'bujur' => '',
+            'nama_wali' => 'nullable|string|max:255',
+            'tahun_lahir_wali' => '' ,
+            'nipd' => 'nullable|string|max:20',
+            'tanggal_masuk_sekolah' => 'required|date',
+            'kebutuhan_khusus_id' => 'nullable|integer',
+            'jenis_tinggal_id' => 'nullable|integer',
+            'alat_transportasi_id' => 'nullable|integer',
+            'anak_keberapa' => 'nullable|integer',
+            'nik_wali' => 'nullable|string|max:16',
+            'nomor_telepon_rumah' => 'nullable|string|max:15',
+            'nomor_telepon_seluler' => 'nullable|string|max:15',
+            'email' => 'nullable|string|email|max:255',
+            'reg_akta_lahir' => 'nullable|string|max:50',
+            'jenjang_pendidikan_ayah' => 'nullable|string|max:50',
+            'pekerjaan_id_ayah' => 'nullable|integer',
+            'penghasilan_id_ayah' => 'nullable|integer',
+            'penghasilan_ayah' => 'nullable|string|max:50',
+            'kebutuhan_khusus_id_ayah' => 'nullable|integer',
+            'jenjang_pendidikan_ibu' => 'nullable|string|max:50',
+            'pekerjaan_id_ibu' => 'nullable|integer',
+            'penghasilan_id_ibu' => 'nullable|integer',
+            'penghasilan_ibu' => 'nullable|string|max:50',
+            'kebutuhan_khusus_id_ibu' => 'nullable|integer',
+            'jenjang_pendidikan_wali' => 'nullable|string|max:50',
+            'pekerjaan_id_wali' => 'nullable|integer',
+            'penghasilan_id_wali' => 'nullable|integer',
+            'id_bank' => 'nullable|integer',
+            'bank' => 'nullable|string|max:50',
+            'nama_kcp' => 'nullable|string|max:255',
+            'rekening_bank' => 'nullable|string|max:50',
+            'rekening_atas_nama' => 'nullable|string|max:255',
+            'jenis_keluar_id' => 'nullable|integer',
+            'jenis_keluar' => 'nullable|string|max:50',
+            'tanggal_keluar' => 'nullable|date',
+            'keterangan_registrasi' => 'nullable|string|max:255',
+            'no_peserta_ujian' => 'nullable|string|max:25',
+            'no_seri_ijazah' => 'nullable|string|max:25',
+            'a_pernah_paud' => '',
+            'a_pernah_tk' => '',
+            'sekolah_asal' => 'nullable|string|max:255',
+            'jenis_pendaftaran_rombel' => 'nullable|string|max:50',
+            'jenis_pendaftaran_rombel_keterangan' => 'nullable|string|max:255',
+            'tingkat_pendidikan_id' => 'nullable|integer',
+            'jurusan_sp_id' => 'nullable|integer',
+            'nama_jurusan' => 'nullable|string|max:255',
+            'jenis_rombel' => 'nullable|string|max:50',
+            'jenis_rombel_keterangan' => 'nullable|string|max:255',
+            'nama_rombel' => 'nullable|string|max:255',
+            'moving_class' => '',
+            'sks' => 'nullable|integer',
+            'registrasi_id' => 'nullable|integer',
+            'id_hobby' => 'nullable|integer',
+            'id_cita' => 'nullable|integer',
+            'tinggi_badan' => '',
+            'berat_badan' => '',
+            'lingkar_kepala' => '',
+            'jarak_rumah_ke_sekolah' => '',
+            'waktu_tempuh_ke_sekolah' => '',
+            'jumlah_saudara_kandung' => 'nullable|integer',
+
             'sekolah_id' => 'required|exists:sekolah,sekolah_id' // Corrected validation rule
         ]);
 
         Pesertadidik::create($validatedData);
 
-          return redirect()->route('admin.showSekolah', ['sekolah_id' => $request->input('sekolah_id')])
-                     ->with('success', 'Peserta didik created successfully.');
+        return redirect()->route('admin.showSekolah', ['sekolah_id' => $request->input('sekolah_id')])
+            ->with('success', 'Peserta didik created successfully.');
     }
 
     public function edit($peserta_didik_id)
--- resources/views/pesertadidik/create.blade.php
+++ resources/views/pesertadidik/create.blade.php
@@ -243,7 +243,340 @@
                                                 <input type="text" id="nama" name="nama" class="form-control"
                                                     value="{{ isset($pesertadidik) ? $pesertadidik->nama : '' }}" required>
                                             </div>
+                                            <div class="form-group col-md-6">
+                                                <label for="tanggal_lahir" class="col-form-label">Tanggal Lahir:</label>
+                                                <input type="date" id="tanggal_lahir" name="tanggal_lahir" class="form-control"
+                                                    value="{{ isset($pesertadidik) ? $pesertadidik->nama : '' }}" required>
+                                            </div>
                                         </div>
+
+                                        <div class="form-row">
+    <div class="form-group col-md-6">
+        <label for="nisn" class="col-form-label">NISN:</label>
+        <input type="text" id="nisn" name="nisn" class="form-control"
+               value="{{ isset($pesertadidik) ? $pesertadidik->nisn : '' }}" required>
+    </div>
+    <div class="form-group col-md-6">
+        <label for="nik" class="col-form-label">NIK:</label>
+        <input type="text" id="nik" name="nik" class="form-control"
+               value="{{ isset($pesertadidik) ? $pesertadidik->nik : '' }}" required>
+    </div>
+</div>
+
+<div class="form-row">
+    <div class="form-group col-md-6">
+        <label for="no_kk" class="col-form-label">No KK:</label>
+        <input type="text" id="no_kk" name="no_kk" class="form-control"
+               value="{{ isset($pesertadidik) ? $pesertadidik->no_kk : '' }}" required>
+    </div>
+    <div class="form-group col-md-6">
+        <label for="tempat_lahir" class="col-form-label">Tempat Lahir:</label>
+        <input type="text" id="tempat_lahir" name="tempat_lahir" class="form-control"
+               value="{{ isset($pesertadidik) ? $pesertadidik->tempat_lahir : '' }}" required>
+    </div>
+</div>
+
+<div class="form-row">
+    <div class="form-group col-md-6">
+        <label for="jenis_kelamin" class="col-form-label">Jenis Kelamin:</label>
+        <select id="jenis_kelamin" name="jenis_kelamin" class="form-control" required>
+            <option value="" disabled selected>Pilih Jenis Kelamin</option>
+            <option value="Laki-laki" {{ isset($pesertadidik) && $pesertadidik->jenis_kelamin == 'Laki-laki' ? 'selected' : '' }}>Laki-laki</option>
+            <option value="Perempuan" {{ isset($pesertadidik) && $pesertadidik->jenis_kelamin == 'Perempuan' ? 'selected' : '' }}>Perempuan</option>
+        </select>
+    </div>
+    <div class="form-group col-md-6">
+        <label for="agama" class="col-form-label">Agama:</label>
+        <input type="text" id="agama" name="agama" class="form-control"
+               value="{{ isset($pesertadidik) ? $pesertadidik->agama : '' }}" >
+    </div>
+</div>
+
+<div class="form-row">
+    <div class="form-group col-md-6">
+        <label for="nama_ayah" class="col-form-label">Nama Ayah:</label>
+        <input type="text" id="nama_ayah" name="nama_ayah" class="form-control"
+               value="{{ isset($pesertadidik) ? $pesertadidik->nama_ayah : '' }}" >
+    </div>
+    <div class="form-group col-md-6">
+        <label for="nik_ayah" class="col-form-label">NIK Ayah:</label>
+        <input type="text" id="nik_ayah" name="nik_ayah" class="form-control"
+               value="{{ isset($pesertadidik) ? $pesertadidik->nik_ayah : '' }}" >
+    </div>
+</div>
+
+<div class="form-row">
+    <div class="form-group col-md-6">
+        <label for="tahun_lahir_ayah" class="col-form-label">Tahun Lahir Ayah:</label>
+        <input type="number" id="tahun_lahir_ayah" name="tahun_lahir_ayah" class="form-control"
+               value="{{ isset($pesertadidik) ? $pesertadidik->tahun_lahir_ayah : '' }}" >
+    </div>
+    <div class="form-group col-md-6">
+        <label for="jenjang_pendidikan_ayah_keterangan" class="col-form-label">Jenjang Pendidikan Ayah:</label>
+        <input type="text" id="jenjang_pendidikan_ayah_keterangan" name="jenjang_pendidikan_ayah_keterangan" class="form-control"
+               value="{{ isset($pesertadidik) ? $pesertadidik->jenjang_pendidikan_ayah_keterangan : '' }}" >
+    </div>
+</div>
+
+<div class="form-row">
+    <div class="form-group col-md-6">
+        <label for="pekerjaan_ayah" class="col-form-label">Pekerjaan Ayah:</label>
+        <input type="text" id="pekerjaan_ayah" name="pekerjaan_ayah" class="form-control"
+               value="{{ isset($pesertadidik) ? $pesertadidik->pekerjaan_ayah : '' }}" >
+    </div>
+    <div class="form-group col-md-6">
+        <label for="nama_ibu_kandung" class="col-form-label">Nama Ibu Kandung:</label>
+        <input type="text" id="nama_ibu_kandung" name="nama_ibu_kandung" class="form-control"
+               value="{{ isset($pesertadidik) ? $pesertadidik->nama_ibu_kandung : '' }}" >
+    </div>
+</div>
+
+<div class="form-row">
+    <div class="form-group col-md-6">
+        <label for="nik_ibu" class="col-form-label">NIK Ibu:</label>
+        <input type="text" id="nik_ibu" name="nik_ibu" class="form-control"
+               value="{{ isset($pesertadidik) ? $pesertadidik->nik_ibu : '' }}" >
+    </div>
+    <div class="form-group col-md-6">
+        <label for="tahun_lahir_ibu" class="col-form-label">Tahun Lahir Ibu:</label>
+        <input type="number" id="tahun_lahir_ibu" name="tahun_lahir_ibu" class="form-control"
+               value="{{ isset($pesertadidik) ? $pesertadidik->tahun_lahir_ibu : '' }}" >
+    </div>
+</div>
+
+<div class="form-row">
+    <div class="form-group col-md-6">
+        <label for="jenjang_pendidikan_ibu_keterangan" class="col-form-label">Jenjang Pendidikan Ibu:</label>
+        <input type="text" id="jenjang_pendidikan_ibu_keterangan" name="jenjang_pendidikan_ibu_keterangan" class="form-control"
+               value="{{ isset($pesertadidik) ? $pesertadidik->jenjang_pendidikan_ibu_keterangan : '' }}" >
+    </div>
+    <div class="form-group col-md-6">
+        <label for="pekerjaan_ibu" class="col-form-label">Pekerjaan Ibu:</label>
+        <input type="text" id="pekerjaan_ibu" name="pekerjaan_ibu" class="form-control"
+               value="{{ isset($pesertadidik) ? $pesertadidik->pekerjaan_ibu : '' }}" >
+    </div>
+</div>
+
+<div class="form-row">
+    <div class="form-group col-md-6">
+        <label for="alamat_jalan" class="col-form-label">Alamat Jalan:</label>
+        <input type="text" id="alamat_jalan" name="alamat_jalan" class="form-control"
+               value="{{ isset($pesertadidik) ? $pesertadidik->alamat_jalan : '' }}" >
+    </div>
+    <div class="form-group col-md-6">
+        <label for="rt" class="col-form-label">RT:</label>
+        <input type="number" id="rt" name="rt" class="form-control"
+               value="{{ isset($pesertadidik) ? $pesertadidik->rt : '' }}" >
+    </div>
+</div>
+
+<div class="form-row">
+    <div class="form-group col-md-6">
+        <label for="rw" class="col-form-label">RW:</label>
+        <input type="number" id="rw" name="rw" class="form-control"
+               value="{{ isset($pesertadidik) ? $pesertadidik->rw : '' }}" >
+    </div>
+    <div class="form-group col-md-6">
+        <label for="nama_dusun" class="col-form-label">Nama Dusun:</label>
+        <input type="text" id="nama_dusun" name="nama_dusun" class="form-control"
+               value="{{ isset($pesertadidik) ? $pesertadidik->nama_dusun : '' }}" >
+    </div>
+</div>
+
+<div class="form-row">
+    <div class="form-group col-md-6">
+        <label for="desa_kelurahan" class="col-form-label">Desa/Kelurahan:</label>
+        <input type="text" id="desa_kelurahan" name="desa_kelurahan" class="form-control"
+               value="{{ isset($pesertadidik) ? $pesertadidik->desa_kelurahan : '' }}" >
+    </div>
+    <div class="form-group col-md-6">
+        <label for="kode_wilayah" class="col-form-label">Kode Wilayah:</label>
+        <input type="text" id="kode_wilayah" name="kode_wilayah" class="form-control"
+               value="{{ isset($pesertadidik) ? $pesertadidik->kode_wilayah : '' }}" >
+    </div>
+</div>
+
+<div class="form-row">
+    <div class="form-group col-md-6">
+        <label for="kode_kecamatan" class="col-form-label">Kode Kecamatan:</label>
+        <input type="text" id="kode_kecamatan" name="kode_kecamatan" class="form-control"
+               value="{{ isset($pesertadidik) ? $pesertadidik->kode_kecamatan : '' }}" >
+    </div>
+    <div class="form-group col-md-6">
+        <label for="kecamatan" class="col-form-label">Kecamatan:</label>
+        <input type="text" id="kecamatan" name="kecamatan" class="form-control"
+               value="{{ isset($pesertadidik) ? $pesertadidik->kecamatan : '' }}" >
+    </div>
+</div>
+
+<div class="form-row">
+    <div class="form-group col-md-6">
+        <label for="kode_kabupaten" class="col-form-label">Kode Kabupaten:</label>
+        <input type="text" id="kode_kabupaten" name="kode_kabupaten" class="form-control"
+               value="{{ isset($pesertadidik) ? $pesertadidik->kode_kabupaten : '' }}" >
+    </div>
+    <div class="form-group col-md-6">
+        <label for="kabupaten" class="col-form-label">Kabupaten:</label>
+        <input type="text" id="kabupaten" name="kabupaten" class="form-control"
+               value="{{ isset($pesertadidik) ? $pesertadidik->kabupaten : '' }}" >
+    </div>
+</div>
+
+<div class="form-row">
+    <div class="form-group col-md-6">
+        <label for="kode_provinsi" class="col-form-label">Kode Provinsi:</label>
+        <input type="text" id="kode_provinsi" name="kode_provinsi" class="form-control"
+               value="{{ isset($pesertadidik) ? $pesertadidik->kode_provinsi : '' }}" >
+    </div>
+    <div class="form-group col-md-6">
+        <label for="provinsi" class="col-form-label">Provinsi:</label>
+        <input type="text" id="provinsi" name="provinsi" class="form-control"
+               value="{{ isset($pesertadidik) ? $pesertadidik->provinsi : '' }}" >
+    </div>
+</div>
+
+<div class="form-row">
+    <div class="form-group col-md-6">
+        <label for="kode_pos" class="col-form-label">Kode Pos:</label>
+        <input type="text" id="kode_pos" name="kode_pos" class="form-control"
+               value="{{ isset($pesertadidik) ? $pesertadidik->kode_pos : '' }}" >
+    </div>
+    <div class="form-group col-md-6">
+        <label for="lintang" class="col-form-label">Lintang:</label>
+        <input type="text" id="lintang" name="lintang" class="form-control"
+               value="{{ isset($pesertadidik) ? $pesertadidik->lintang : '' }}" >
+    </div>
+</div>
+
+<div class="form-row">
+    <div class="form-group col-md-6">
+        <label for="bujur" class="col-form-label">Bujur:</label>
+        <input type="text" id="bujur" name="bujur" class="form-control"
+               value="{{ isset($pesertadidik) ? $pesertadidik->bujur : '' }}" >
+    </div>
+    <div class="form-group col-md-6">
+        <label for="nama_wali" class="col-form-label">Nama Wali:</label>
+        <input type="text" id="nama_wali" name="nama_wali" class="form-control"
+               value="{{ isset($pesertadidik) ? $pesertadidik->nama_wali : '' }}" >
+    </div>
+</div>
+
+<div class="form-row">
+    <div class="form-group col-md-6">
+        <label for="tahun_lahir_wali" class="col-form-label">Tahun Lahir Wali:</label>
+        <input type="number" id="tahun_lahir_wali" name="tahun_lahir_wali" class="form-control"
+               value="{{ isset($pesertadidik) ? $pesertadidik->tahun_lahir_wali : '' }}" >
+    </div>
+    <div class="form-group col-md-6">
+        <label for="nipd" class="col-form-label">NIPD:</label>
+        <input type="text" id="nipd" name="nipd" class="form-control"
+               value="{{ isset($pesertadidik) ? $pesertadidik->nipd : '' }}" >
+    </div>
+</div>
+
+<div class="form-row">
+    <div class="form-group col-md-6">
+        <label for="tanggal_masuk_sekolah" class="col-form-label">Tanggal Masuk Sekolah:</label>
+        <input type="date" id="tanggal_masuk_sekolah" name="tanggal_masuk_sekolah" class="form-control"
+               value="{{ isset($pesertadidik) ? $pesertadidik->tanggal_masuk_sekolah : '' }}" >
+    </div>
+    <div class="form-group col-md-6">
+        <label for="kebutuhan_khusus" class="col-form-label">Kebutuhan Khusus:</label>
+        <input type="text" id="kebutuhan_khusus" name="kebutuhan_khusus" class="form-control"
+               value="{{ isset($pesertadidik) ? $pesertadidik->kebutuhan_khusus : '' }}" >
+    </div>
+</div>
+<div class="form-row">
+    <div class="form-group col-md-6">
+        <label for="jenis_tinggal" class="col-form-label">Jenis Tinggal:</label>
+        <input type="text" id="jenis_tinggal" name="jenis_tinggal" class="form-control"
+               value="{{ isset($pesertadidik) ? $pesertadidik->jenis_tinggal : '' }}" >
+    </div>
+    <div class="form-group col-md-6">
+        <label for="alat_transportasi" class="col-form-label">Alat Transportasi:</label>
+        <input type="text" id="alat_transportasi" name="alat_transportasi" class="form-control"
+               value="{{ isset($pesertadidik) ? $pesertadidik->alat_transportasi : '' }}" >
+    </div>
+</div>
+
+<div class="form-row">
+    <div class="form-group col-md-6">
+        <label for="anak_keberapa" class="col-form-label">Anak Keberapa:</label>
+        <input type="number" id="anak_keberapa" name="anak_keberapa" class="form-control"
+               value="{{ isset($pesertadidik) ? $pesertadidik->anak_keberapa : '' }}" >
+    </div>
+    <div class="form-group col-md-6">
+        <label for="nik_wali" class="col-form-label">NIK Wali:</label>
+        <input type="text" id="nik_wali" name="nik_wali" class="form-control"
+               value="{{ isset($pesertadidik) ? $pesertadidik->nik_wali : '' }}" >
+    </div>
+</div>
+
+<div class="form-row">
+    <div class="form-group col-md-6">
+        <label for="nomor_telepon_rumah" class="col-form-label">Nomor Telepon Rumah:</label>
+        <input type="text" id="nomor_telepon_rumah" name="nomor_telepon_rumah" class="form-control"
+               value="{{ isset($pesertadidik) ? $pesertadidik->nomor_telepon_rumah : '' }}">
+    </div>
+    <div class="form-group col-md-6">
+        <label for="nomor_telepon_seluler" class="col-form-label">Nomor Telepon Seluler:</label>
+        <input type="text" id="nomor_telepon_seluler" name="nomor_telepon_seluler" class="form-control"
+               value="{{ isset($pesertadidik) ? $pesertadidik->nomor_telepon_seluler : '' }}" >
+    </div>
+</div>
+
+<div class="form-row">
+    <div class="form-group col-md-6">
+        <label for="email" class="col-form-label">Email:</label>
+        <input type="email" id="email" name="email" class="form-control"
+               value="{{ isset($pesertadidik) ? $pesertadidik->email : '' }}">
+    </div>
+    <div class="form-group col-md-6">
+        <label for="reg_akta_lahir" class="col-form-label">Registrasi Akta Lahir:</label>
+        <input type="text" id="reg_akta_lahir" name="reg_akta_lahir" class="form-control"
+               value="{{ isset($pesertadidik) ? $pesertadidik->reg_akta_lahir : '' }}">
+    </div>
+</div>
+
+<div class="form-row">
+    <div class="form-group col-md-6">
+        <label for="tinggi_badan" class="col-form-label">Tinggi Badan (cm):</label>
+        <input type="number" id="tinggi_badan" name="tinggi_badan" class="form-control"
+               value="{{ isset($pesertadidik) ? $pesertadidik->tinggi_badan : '' }}" >
+    </div>
+    <div class="form-group col-md-6">
+        <label for="berat_badan" class="col-form-label">Berat Badan (kg):</label>
+        <input type="number" id="berat_badan" name="berat_badan" class="form-control"
+               value="{{ isset($pesertadidik) ? $pesertadidik->berat_badan : '' }}" >
+    </div>
+</div>
+
+<div class="form-row">
+    <div class="form-group col-md-6">
+        <label for="lingkar_kepala" class="col-form-label">Lingkar Kepala (cm):</label>
+        <input type="number" id="lingkar_kepala" name="lingkar_kepala" class="form-control"
+               value="{{ isset($pesertadidik) ? $pesertadidik->lingkar_kepala : '' }}">
+    </div>
+    <div class="form-group col-md-6">
+        <label for="jarak_rumah_ke_sekolah_km" class="col-form-label">Jarak Rumah ke Sekolah (km):</label>
+        <input type="number" id="jarak_rumah_ke_sekolah_km" name="jarak_rumah_ke_sekolah_km" class="form-control"
+               value="{{ isset($pesertadidik) ? $pesertadidik->jarak_rumah_ke_sekolah_km : '' }}" >
+    </div>
+</div>
+
+<div class="form-row">
+    <div class="form-group col-md-6">
+        <label for="menit_tempuh_ke_sekolah" class="col-form-label">Waktu Tempuh ke Sekolah (menit):</label>
+        <input type="number" id="menit_tempuh_ke_sekolah" name="menit_tempuh_ke_sekolah" class="form-control"
+               value="{{ isset($pesertadidik) ? $pesertadidik->menit_tempuh_ke_sekolah : '' }}" >
+    </div>
+    <div class="form-group col-md-6">
+        <label for="jumlah_saudara_kandung" class="col-form-label">Jumlah Saudara Kandung:</label>
+        <input type="number" id="jumlah_saudara_kandung" name="jumlah_saudara_kandung" class="form-control"
+               value="{{ isset($pesertadidik) ? $pesertadidik->jumlah_saudara_kandung : '' }}" >
+    </div>
+</div>
+
                                     
                                         <input type="hidden" name="sekolah_id" value="{{ $sekolah_id }}">
                                     
